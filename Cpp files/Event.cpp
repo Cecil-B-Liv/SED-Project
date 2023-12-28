da332b787df12a9fd5a3addc9d624f6501a1f225
Milkyway Galaxy <blannkisblank@gmail.com>
2023-12-28 23:05:50 +0700

    added Event class for menu and login

---
+++ Cpp files/Event.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Nick Anderson on 28/12/2023.
+//
+
+#include "../Header files/Event.h"
+#include "../Header files/BasicSTDLIB.h"
+
+#define LOGIN 1
+
+void Event::Login() {
+    string username;
+    string password;
+
+    cout << "Enter your username: ";
+    getline(cin >> std::ws, username);
+
+    cout << "Enter your password: ";
+    getline(cin >> std::ws, password);
+
+    if ((username == "liv") && (password == "LIV")) {
+        cout << "Welcome";
+    }
+}
+
+void Event::Menu() {
+    int choice;
+
+    cout << "What do you want\n";
+    cout << "1. Login\n2. Register\n";
+
+    cin >> choice;
+
+    if (choice == LOGIN) {
+        Login();
+    }
+}
---
+++ Header files/Event.h
@@ -0,0 +1,15 @@
+//
+// Created by Nick Anderson on 28/12/2023.
+//
+
+#ifndef SED_PROJECT_EVENT_H
+#define SED_PROJECT_EVENT_H
+
+class Event {
+public:
+    static void Login();
+
+    static void Menu();
+};
+
+#endif //SED_PROJECT_EVENT_H
