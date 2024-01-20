067d0dba2290ce7c15caf4eee4bac7f09311cb13
Milkyway Galaxy <blannkisblank@gmail.com>
2024-01-21 01:47:04 +0700

    Update Rating.cpp

--- Cpp files/Rating.cpp
+++ Cpp files/Rating.cpp
@@ -21,37 +21,3 @@ void Rating::showInfo() {
     cout << "Host Rating: " << hostRating;
     cout << "Comment: " << comments;
 }
-
-// void addReview(Member &member)
-// {
-//     double sup_score = 0;
-//     double skill_score = 0;
-//     double host_score = 0;
-//     string comment;
-//     if (member.isHost == true)
-//     {
-//         std::cout << "Enter a  score for the supporter's performance (1 - 5): ";
-//         std::cin >> sup_score;
-//         std::cin.ignore(); // ignore newline character
-//         std::cout << "Enter a score for the supporter's skill (1 - 5): ";
-//         std::cin >> skill_score;
-//         std::cin.ignore(); // ignore newline character
-//         std::cout << "Enter a comment for the supporter: ";
-//         std::getline(std::cin, comment);
-//         std::cout << "Host " << m.getFullName() << " rate the supporter " << sup_score << " for performance and "
-//                   << skill_score << " for skill of the supporter";
-//         std::cout << " with the comment: "
-//                   << comment << std::endl;
-//     }
-//     else
-//     {
-//         std::cout << "Enter a score for the host (1 - 5): ";
-//         std::cin >> host_score;
-//         std::cin.ignore(); // ignore newline character
-//         std::cout << "Enter a comment for the host: ";
-//         std::getline(std::cin, comment);
-//         std::cout << "Supporter " << m.getFullName() << " rated the host: " << host_score;
-//         std::cout << " , with the comment: "
-//                   << comment << "." << std::endl;
-//     }
-// }
